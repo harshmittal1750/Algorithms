3430740b73686f85b53594793022bd82450e7a27
pro-tyush <pratyushedu2510@gmail.com>
2020-06-25 15:49:35 +0530

    upto week2-2

--- FibonacciLastDigit.java
+++ FibonacciLastDigit.java
@@ -1,27 +1,38 @@
 import java.util.*;
 
 public class FibonacciLastDigit {
-    private static int getFibonacciLastDigitNaive(int n) {
-        if (n <= 1)
-            return n;
+//If you write out a sequence of Fibonacci numbers, you can see that the last digits repeat every 60 numbers.
+//The 61st Fibonacci number is 2504730781961. The 62nd is 4052739537881
+//This algorithm can take really large numbers and calculate the last digit using the pattern stated above
 
-        int previous = 0;
-        int current  = 1;
 
-        for (int i = 0; i < n - 1; ++i) {
-            int tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
+    private static int[] Fib60TermsInArray() {
+        //Storing last digit of 60 terms of series in array
+        int[] fibLast = new int[60];
+        fibLast[0] = 0;
+        fibLast[1] = 1;
+        for (int i = 2; i < fibLast.length; i++) {
+            fibLast[i] = (fibLast[i - 1] + fibLast[i - 2]) % 10;
         }
-
-        return current % 10;
+        return fibLast;
     }
-    
+
+    private static int getFibonacciLastDigitUsingPattern(int n) {
+
+        int[] fibLast = Fib60TermsInArray();
+        if (n <= 60) {
+            return fibLast[n];
+        }
+        int index = n % 60;
+        return fibLast[index];
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
-        System.out.println(c);
+        System.out.println(getFibonacciLastDigitUsingPattern(n));
     }
+
 }
+
 
